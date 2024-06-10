e92a0f36826cb10f6e41824bcbb477c9dc3d3dc5
Jeff <albert.anderson@gmail.com>
2024-06-09 17:13:27 -0700

    Update fafollow.js

--- fafollow.js
+++ fafollow.js
@@ -14,15 +14,15 @@ app.get('/', (req, res) => {
             <!-- Farcaster meta tags -->
             <meta property="fc:frame" content="vNext" />
             <meta property="fc:frame:image" content="https://fc-dev-call.replit.app/image" />
-            <meta property="fc:frame:button:1" content="Spec" />
+            <meta property="fc:frame:button:1" content="Website" />
             <meta property="fc:frame:button:1:action" content="link" />
-            <meta property="fc:frame:button:1:target" content="https://docs.farcaster.xyz/reference/frames/spec#constructing-a-frame" />
-            <meta property="fc:frame:button:2" content="Image" />
+            <meta property="fc:frame:button:1:target" content="https://ens.auction/" />
+            <meta property="fc:frame:button:2" content="X" />
             <meta property="fc:frame:button:2:action" content="link" />
-            <meta property="fc:frame:button:2:target" content="https://fc-dev-call.replit.app/image" />
-            <meta property="fc:frame:button:3" content="Frame Validator" />
+            <meta property="fc:frame:button:2:target" content="https://x.com/ens_auction" />
+            <meta property="fc:frame:button:3" content="Telegram" />
             <meta property="fc:frame:button:3:action" content="link" />
-            <meta property="fc:frame:button:3:target" content="https://warpcast.com/~/developers/frames" />
+            <meta property="fc:frame:button:3:target" content="https://t.me/ENS_Auctions" />
             </head>
         <body>
             <h1>Hello, World!</h1>
